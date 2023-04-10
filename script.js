d9c9aa9a69d7cb83ed6c41711dbc637196de744e
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-04-10 13:12:09 +0200

    Button selector so player can be X or O

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
-function putt(squareQuerySelector)
+function putMark(squareQuerySelector)
 {
-    squareQuerySelector.innerHTML = mark
-    mark == "X" ? mark = "O" : mark="X"
+    if (player1Mark == "X")
+    {
+        if (tour == 0 || tour == 2 || tour ==  4|| tour == 6 || tour == 8)        
+        {
+            squareQuerySelector.innerHTML = player1Mark
+        }
+        else
+        {
+            squareQuerySelector.innerHTML = player2Mark   
+        }        
+    }
+    else
+    {
+        if (tour == 0 || tour == 2 || tour ==  4|| tour == 6 || tour == 8)        
+        {
+            squareQuerySelector.innerHTML = player1Mark
+        }
+        else
+        {
+            squareQuerySelector.innerHTML = player2Mark  
+        }        
+    }
+}
+function player1chooseX()
+{
+    player1Mark = "X"
+    player2Mark = "O"
+    console.log("isse")
+    return [player1Mark, player2Mark]
+}
+function player1chooseO()
+{
+    player1Mark = "O"
+    player2Mark = "X"
+    console.log("ayay")
+    return [player1Mark, player2Mark]
 }
 
+let player1Mark 
+let player2Mark
 const squareQuerySelector = document.querySelectorAll(".square")
+const xMarkQuerySelector = document.querySelector(".Xbutton")
+const oMarkQuerySelector = document.querySelector(".Obutton")
 const xMarkQuery = squareQuerySelector.forEach(squareQuerySelector =>    
     {
-        squareQuerySelector.addEventListener("click", () => putt(squareQuerySelector) )
+        squareQuerySelector.addEventListener("click", () => putMark(squareQuerySelector))
     })
-var mark = "X"
+const xButtonClick = xMarkQuerySelector.addEventListener("click", player1chooseX)
+const oButtonClick = oMarkQuerySelector.addEventListener("click", player1chooseO)
+let tour = 0
