c692612be49cd083c96ee6144211d8b9df85d994
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-04-10 16:37:34 +0200

    Some comments

--- script.js
+++ script.js
@@ -53,38 +53,44 @@ function player1chooseO()
     return [player1Mark, player2Mark]
 }
 
-function winSituation()
+function winSituation() 
 {
-    if ((document.querySelector(".sq1").innerHTML == "O" && document.querySelector(".sq2").innerHTML== "O" && document.querySelector(".sq3").innerHTML == "O") ||
+    if (((document.querySelector(".sq1").innerHTML == "O" && document.querySelector(".sq2").innerHTML== "O" && document.querySelector(".sq3").innerHTML == "O") ||
         (document.querySelector(".sq4").innerHTML == "O" && document.querySelector(".sq5").innerHTML== "O" && document.querySelector(".sq6").innerHTML == "O") ||
         (document.querySelector(".sq7").innerHTML == "O" && document.querySelector(".sq8").innerHTML== "O" && document.querySelector(".sq9").innerHTML == "O") ||
         (document.querySelector(".sq1").innerHTML == "O" && document.querySelector(".sq5").innerHTML == "O" && document.querySelector(".sq9").innerHTML == "O")||
         (document.querySelector(".sq3").innerHTML == "O" && document.querySelector(".sq5").innerHTML == "O" && document.querySelector(".sq7").innerHTML == "O")||        
         (document.querySelector(".sq1").innerHTML == "O" && document.querySelector(".sq4").innerHTML == "O" && document.querySelector(".sq7").innerHTML == "O")||
         (document.querySelector(".sq2").innerHTML == "O" && document.querySelector(".sq5").innerHTML == "O" && document.querySelector(".sq8").innerHTML == "O")||
-        (document.querySelector(".sq3").innerHTML == "O" && document.querySelector(".sq6").innerHTML == "O" && document.querySelector(".sq9").innerHTML == "O")
-        )
+        (document.querySelector(".sq3").innerHTML == "O" && document.querySelector(".sq6").innerHTML == "O" && document.querySelector(".sq9").innerHTML == "O"))
+        && active == true)
     {
         alert(" O : You W I N")
+        active =false           //Ends the game
     }
-    else if ((document.querySelector(".sq1").innerHTML == "X" && document.querySelector(".sq2").innerHTML== "X" && document.querySelector(".sq3").innerHTML == "X") ||
+    else if (((document.querySelector(".sq1").innerHTML == "X" && document.querySelector(".sq2").innerHTML== "X" && document.querySelector(".sq3").innerHTML == "X") ||
     (document.querySelector(".sq4").innerHTML == "X" && document.querySelector(".sq5").innerHTML== "X" && document.querySelector(".sq6").innerHTML == "X") ||
     (document.querySelector(".sq7").innerHTML == "X" && document.querySelector(".sq8").innerHTML== "X" && document.querySelector(".sq9").innerHTML == "X") ||
     (document.querySelector(".sq1").innerHTML == "X" && document.querySelector(".sq5").innerHTML == "X" && document.querySelector(".sq9").innerHTML == "X")||
     (document.querySelector(".sq3").innerHTML == "X" && document.querySelector(".sq5").innerHTML == "X" && document.querySelector(".sq7").innerHTML == "X")||        
     (document.querySelector(".sq1").innerHTML == "X" && document.querySelector(".sq4").innerHTML == "X" && document.querySelector(".sq7").innerHTML == "X")||
     (document.querySelector(".sq2").innerHTML == "X" && document.querySelector(".sq5").innerHTML == "X" && document.querySelector(".sq8").innerHTML == "X")||
-    (document.querySelector(".sq3").innerHTML == "X" && document.querySelector(".sq6").innerHTML == "X" && document.querySelector(".sq9").innerHTML == "X")
-    )
+    (document.querySelector(".sq3").innerHTML == "X" && document.querySelector(".sq6").innerHTML == "X" && document.querySelector(".sq9").innerHTML == "X"))
+    && active == true)
     {
         alert("X : You W I N")
+        active = false          //Ends the game
     }
 }
+//________________________________________________VARIABLES_____________________________________________________
 let mark = ["X","O"]
 let player1Mark 
 let player2Mark
 let active = false                                                      //The player has to choose X or O first to begin the game
 let turn = 0
+let placeholder                                                         //Placeholder is the turn counter
+let turnMessage
+//_________________________________________________CONST___________________________________________________________
 const squareQuerySelector = document.querySelectorAll(".square")
 const xMarkQuerySelector = document.querySelector(".Xbutton")
 const oMarkQuerySelector = document.querySelector(".Obutton")
@@ -98,5 +104,3 @@ const cells = [...document.querySelectorAll(".square")]
 const xButtonClick = xMarkQuerySelector.addEventListener("click", player1chooseX)
 const oButtonClick = oMarkQuerySelector.addEventListener("click", player1chooseO)
 console.log(cells)
-let placeholder                                                         //Placeholder is the turn counter
-let turnMessage
